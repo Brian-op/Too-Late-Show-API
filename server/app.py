04156111a90cc477dbbfc16cbd33a47bf5bc2452
Brian <brian.kiprono8@student.moringaschool.com>
2025-06-24 00:36:44 +0300

    register blueprints to app

--- server/app.py
+++ server/app.py
@@ -17,6 +17,14 @@ def create_app():
     from server.models.episode import Episode
     from server.models.appearance import Appearance
     
+    from server.controllers.guest_controller import guest_bp
+    from server.controllers.episode_controller import episode_bp
+    from server.controllers.appearance_controller import appearance_bp  
+
+    app.register_blueprint(guest_bp)
+    app.register_blueprint(episode_bp)
+    app.register_blueprint(appearance_bp)
+    
     db.init_app(app)
     migrate.init_app(app,db)
     jwt.init_app(app)
